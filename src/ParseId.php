91e4ba9c796a1dba172e235eda75f1adef42893e
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-03-01 13:58:37 +0200

    Create ParseId.php

---
+++ src/ParseId.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Bermuda\HTTP;
+
+final class ParseId implements ParserInterface
+{
+    public function __construct(
+      public readonly string $delimiter = ','
+    ) {
+    }
+
+    /**
+     * @params string $ids
+     * @return int[]
+     * @throws ParserException
+     */
+    public function parse(string $ids): array
+    {
+        foreach (explode($this->delimiter, $ids) as $id) {
+            if (!is_numeric($id)) throw new ParserException('Id is not numeric');
+            $arr[] = $id + 0;
+        }
+
+        return $arr ?? [];
+    }
+}
